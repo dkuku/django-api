39f34918c39ab5b154b92418c5e0992c1851ad66
Daniel Kukula <daniel.kuku@gmail.com>
2019-09-04 21:21:29 +0100

    create recipes with connected fields

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -47,5 +47,5 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return self.serializer_class
 
     def perform_create(self, serializer):
-        """Create a new attribute"""
+        """Create a new recipe"""
         serializer.save(user=self.request.user)
